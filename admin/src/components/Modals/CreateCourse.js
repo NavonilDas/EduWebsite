b8d43e7042b51f2bc406de3d0cc25bb3ca3d35cb
Navonil Das <navneeladas@gmail.com>
2020-11-03 22:31:42 +0530

    Create Course Form

--- admin/src/components/Modals/CreateCourse.js
+++ admin/src/components/Modals/CreateCourse.js
@@ -1,15 +1,24 @@
 import { Button, Checkbox, FormControlLabel, TextField } from '@material-ui/core';
 import { DropzoneDialog } from 'material-ui-dropzone';
 import React from 'react';
-import NavBar from './NavBar';
 
 class CreateCourse extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             open: false,
-            files: []
+            files: [],
+            title: "",
+            description: "",
+            isPaid: true,
+            price: 0,
+            duration: 10
         };
+        this.handleClose = this.handleClose.bind(this);
+        this.handleSave = this.handleSave.bind(this);
+        this.handleOpen = this.handleOpen.bind(this);
+        this.submit = this.submit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
     handleClose() {
@@ -31,23 +40,50 @@ class CreateCourse extends React.Component {
             open: true,
         });
     }
-    
+    handleChange(event) {
+        const ele = event.target;
+        let tmp = {};
+        if (ele.type && ele.type == 'checkbox') {
+            tmp[ele.name] = ele.checked;
+        } else {
+            tmp[ele.name] = ele.value;
+        }
+        this.setState(tmp);
+    }
+    submit() {
+        // TODO: API Request
+        console.log(this.state);
+        if (this.props.onClose) {
+            this.props.onClose();
+        }
+    }
+
     render() {
         return (
-            <main className="admin-content">
-                <NavBar title="Create Course" />
-                <div className="admin-body">
+            <main className="create-course modal">
+                <h1>Create Course</h1>
+                <div>
                     <form className="course-form" style={{ width: "300px" }}>
-                        <TextField required id="course-title" label="Course Title" />
+                        <TextField
+                            required
+                            name="title"
+                            id="course-title"
+                            label="Course Title"
+                            value={this.state.title}
+                            onChange={this.handleChange}
+                        />
+
                         <TextField
                             id="outlined-multiline-static"
                             label="Course Description"
                             multiline
                             rows={4}
+                            name="description"
                             variant="outlined"
+                            value={this.state.description}
+                            onChange={this.handleChange}
                         />
 
-                        <TextField required id="course-slug" label="Course url" />
                         <FormControlLabel
                             control={
                                 <Checkbox
@@ -55,27 +91,57 @@ class CreateCourse extends React.Component {
                                     color="primary"
                                 />
                             }
+                            checked={this.state.isPaid}
+                            onChange={this.handleChange}
                             label="Is Paid"
                         />
                         <TextField
                             id="filled-number"
                             label="Price"
                             type="number"
+                            name="price"
+                            value={this.state.price}
+                            onChange={this.handleChange}
                         />
+                        <div className="d-flex">
+                            <TextField
+                                id="filled-number"
+                                label="Course Duration"
+                                type="number"
+                                name="duration"
+                                onChange={this.handleChange}
+                                value={this.state.duration}
+                            />
+                            <label style={{ margin: "auto 0 0 10px", fontSize: "1.5em" }}>Months</label>
+                            {/* <FormControl>
+                                <InputLabel id="demo-simple-select-helper-label">Du</InputLabel>
+                                <Select
+                                    labelId="demo-simple-select-helper-label"
+                                    id="demo-simple-select-helper"
+                                    value={10}>
+                                    <MenuItem value="">
+                                        <em>None</em>
+                                    </MenuItem>
+                                    <MenuItem value={10}>Ten</MenuItem>
+                                    <MenuItem value={20}>Twenty</MenuItem>
+                                    <MenuItem value={30}>Thirty</MenuItem>
+                                </Select>
+                            </FormControl> */}
+                        </div>
                         <div>
-                            <Button onClick={this.handleOpen.bind(this)}>
+                            <Button onClick={this.handleOpen} variant="contained">
                                 Add Thumbnail
                             </Button>
                             <DropzoneDialog
                                 open={this.state.open}
-                                onSave={this.handleSave.bind(this)}
+                                onSave={this.handleSave}
                                 acceptedFiles={['image/jpeg', 'image/png', 'image/bmp']}
                                 showPreviews={true}
                                 maxFileSize={5000000}
-                                onClose={this.handleClose.bind(this)}
+                                onClose={this.handleClose}
                             />
                         </div>
-                        <Button variant="contained" color="primary">
+                        <Button variant="contained" color="primary" onClick={this.submit}>
                             Submit
                         </Button>
                     </form>
