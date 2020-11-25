d9a561884a36c3bb3bd50613783d73079eed5b80
Navonil Das <navneeladas@gmail.com>
2020-11-25 12:01:12 +0530

    Dashboard Chainging Button with icons

--- admin/src/components/Dashboard.js
+++ admin/src/components/Dashboard.js
@@ -7,8 +7,12 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import { Modal } from '@material-ui/core';
+import { IconButton, Modal } from '@material-ui/core';
 import CreateCategory from './Modals/CreateCategory';
+
+import ShareIcon from '@material-ui/icons/Share';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
 
 class Dashboard extends React.Component {
     constructor(props) {
@@ -61,6 +65,36 @@ class Dashboard extends React.Component {
                 title: "Class 12",
                 slug: "programming-in-c"
             },
+            {
+                id: 'abc',
+                thumbnail: "https://prod-discovery.edx-cdn.org/media/course/image/bb5e9463-0248-4f78-a337-b8bb9d829f2b-a71b8e897830.small.jpeg",
+                title: "Class 7",
+                slug: "programming-in-c"
+            },
+            {
+                id: 'abc',
+                thumbnail: "https://prod-discovery.edx-cdn.org/media/course/image/bb5e9463-0248-4f78-a337-b8bb9d829f2b-a71b8e897830.small.jpeg",
+                title: "Class 8",
+                slug: "programming-in-c"
+            },
+            {
+                id: 'abc',
+                thumbnail: "https://prod-discovery.edx-cdn.org/media/course/image/bb5e9463-0248-4f78-a337-b8bb9d829f2b-a71b8e897830.small.jpeg",
+                title: "Class 9",
+                slug: "programming-in-c"
+            },
+            {
+                id: 'abc',
+                thumbnail: "https://prod-discovery.edx-cdn.org/media/course/image/bb5e9463-0248-4f78-a337-b8bb9d829f2b-a71b8e897830.small.jpeg",
+                title: "Class 10",
+                slug: "programming-in-c"
+            },
+            {
+                id: 'abc',
+                thumbnail: "https://prod-discovery.edx-cdn.org/media/course/image/bb5e9463-0248-4f78-a337-b8bb9d829f2b-a71b8e897830.small.jpeg",
+                title: "Class 12",
+                slug: "programming-in-c"
+            },
         ];
         return (
             <main className="admin-content">
@@ -75,30 +109,39 @@ class Dashboard extends React.Component {
                     <div className="row">
                         {
                             items.map((ele, ind) => (
-                                <div className="col-md-2" key={`card-${ind}`} style={{ padding: "15px" }}>
+                                <div className="col-md-2" key={`card-${ind}`} style={{ marginBottom: "30px" }}>
                                     <Card>
-                                        <CardActionArea>
+
+                                        <CardActionArea onClick={() => {
+                                            this.props.history.push(`/category/${ele.id}`);
+                                        }}>
                                             <CardMedia
                                                 className="card-img"
                                                 image={ele.thumbnail}
                                                 title={ele.title}
                                             />
-                                            <CardContent>
+                                            <CardContent style={{ paddingBottom: 0 }}>
                                                 <Typography gutterBottom variant="h5" component="h2">
                                                     {ele.title}
                                                 </Typography>
                                             </CardContent>
-
                                         </CardActionArea>
 
-                                        <CardActions>
-                                            {/* <Button size="small" color="primary">
-                                                Share
-                                            </Button> */}
-                                            <Button size="small" color="primary" href={`/category/${ele.id}`}>
+                                        <CardActions style={{ paddingTop: 0 }}>
+                                            <IconButton aria-label="Edit" style={{ color: "#097d01" }}>
+                                                <EditIcon />
+                                            </IconButton>
+                                            <IconButton aria-label="Delete" style={{ color: "#db3825" }} >
+                                                <DeleteIcon />
+                                            </IconButton>
+                                            <IconButton aria-label="Share" style={{ color: "#1d70f5" }}>
+                                                <ShareIcon />
+                                            </IconButton>
+                                            {/* <Button size="small" color="primary" href={``}>
                                                 Edit
-                                            </Button>
+                                            </Button> */}
                                         </CardActions>
+
                                     </Card>
                                 </div>
                             ))
