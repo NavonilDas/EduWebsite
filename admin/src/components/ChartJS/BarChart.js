9033d8d61ac179373d27468c06bd93cbf892bd6e
Navonil Das <navneeladas@gmail.com>
2020-12-09 11:07:45 +0530

    BarChart Component

---
+++ admin/src/components/ChartJS/BarChart.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Chart from "chart.js";
+
+class BarChart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.canvas = React.createRef();
+    this.chart = null;
+  }
+
+  componentDidMount() {
+    this.chart = new Chart(this.canvas.current, {
+      type: "bar",
+      data: {
+        labels: this.props.labels || [],
+        datasets: [
+          {
+            label: this.props.title || "",
+            data: this.props.data || [10, 20, 30, 40, 50, 60, 70, 80],
+            backgroundColor:
+              this.props.backgroundColor || "rgb(155, 205, 206,0.5)",
+            borderColor: this.props.borderColor || "rgb(155, 205, 206)",
+            borderWidth: this.props.borderWidth || 1,
+          },
+        ],
+      },
+    });
+  }
+
+  render() {
+    return (
+      <canvas
+        ref={this.canvas}
+        style={{
+          height: this.props.height || "300px",
+          width: this.props.width || "300px",
+        }}
+      />
+    );
+  }
+}
+
+export default BarChart;
