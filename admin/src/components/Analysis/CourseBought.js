d246cb38423f0b8253c554a8fc28cfe6bd799372
Navonil Das <navneeladas@gmail.com>
2020-12-09 19:21:43 +0530

    CourseBought Analysis Component

---
+++ admin/src/components/Analysis/CourseBought.js
@@ -0,0 +1,113 @@
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Paper,
+  Select,
+  Typography,
+} from "@material-ui/core";
+import React from "react";
+import BarChart from "../ChartJS/BarChart";
+
+import axios from "axios";
+import API, { errorHandler } from "../../Api";
+const { HOST } = API;
+
+class CourseBought extends React.Component {
+  static weeks = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  constructor(props) {
+    super(props);
+    this.state = {
+      labels: [],
+      title: "",
+      selected: "10",
+      data: [],
+    };
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  componentDidMount() {
+    const date = new Date();
+    const time = `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
+    axios
+      .get(`${HOST}analysis/weekly?time=${time}`, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        this.setState({
+          data: res.data.data,
+          title: res.data.title,
+          labels: res.data.labels,
+        });
+      })
+      .catch(errorHandler);
+  }
+
+  onChange(event) {
+    const { value } = event.target;
+    this.setState({ selected: value, labels: [] });
+
+    const date = new Date();
+    const time = `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
+    let request = null;
+    if (value === "20") {
+      request = axios.get(`${HOST}analysis/monthly?time=${time}`, {
+        withCredentials: true,
+      });
+    } else if (value === "30") {
+      request = axios.get(`${HOST}analysis/yearly?time=${time}`, {
+        withCredentials: true,
+      });
+    } else {
+      request = axios.get(`${HOST}analysis/weekly?time=${time}`, {
+        withCredentials: true,
+      });
+    }
+    request
+      .then((res) => {
+        this.setState({
+          data: res.data.data,
+          title: res.data.title,
+          labels: res.data.labels,
+        });
+      })
+      .catch(errorHandler);
+  }
+
+  render() {
+    return (
+      <Paper style={{ padding: "16px" }}>
+        <Typography variant="h4" gutterBottom>
+          Course Bought
+        </Typography>
+
+        <FormControl>
+          <InputLabel id="course-bought-select-id">Select</InputLabel>
+          <Select
+            labelId="course-bought-select-id"
+            value={this.state.selected}
+            onChange={this.onChange}
+          >
+            <MenuItem value="10">Weekly</MenuItem>
+            <MenuItem value="20">Montly</MenuItem>
+            <MenuItem value="30">Yearly</MenuItem>
+          </Select>
+        </FormControl>
+
+        {this.state.labels.length > 0 ? (
+          <BarChart
+            width="450px"
+            title={this.state.title}
+            labels={this.state.labels}
+            data={this.state.data}
+          />
+        ) : (
+          ""
+        )}
+      </Paper>
+    );
+  }
+}
+
+export default CourseBought;
