e9cf762b31d048f7ba9f80280865f0fd4750a80e
Navonil Das <navneeladas@gmail.com>
2020-12-10 09:31:47 +0530

    Chart Js Line Plot

---
+++ admin/src/components/ChartJS/Line.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Chart from "chart.js";
+
+class Line extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.chart = null;
+    this.canvas = React.createRef();
+  }
+
+  componentDidMount() {
+    this.chart = new Chart(this.canvas.current, {
+      type: "line",
+      data: {
+        labels: this.props.labels || [],
+        datasets: [
+          {
+            label: this.props.title || "",
+            data: this.props.data || [],
+            backgroundColor:
+              this.props.backgroundColor || "rgb(155, 205, 206,0.5)",
+            borderColor: this.props.borderColor || "rgb(155, 205, 206)",
+            borderWidth: this.props.borderWidth || 1,
+          },
+        ],
+      },
+    });
+  }
+
+  render() {
+    return (
+      <canvas
+        ref={this.canvas}
+        style={{
+          height: this.props.height || "300px",
+          width: this.props.width || "300px",
+        }}
+      />
+    );
+  }
+}
+
+export default Line;
