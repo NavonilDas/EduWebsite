edd03bb19c0da96e6d9f596e3e66492140a1525a
Navonil Das <navneeladas@gmail.com>
2020-12-11 14:11:45 +0530

    Starting with Income Component

---
+++ admin/src/components/Analysis/Income.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Income extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Income;
