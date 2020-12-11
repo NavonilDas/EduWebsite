046ff96a295dddd48b63ca20166f7353352ffce6
Navonil Das <navneeladas@gmail.com>
2020-12-11 15:05:31 +0530

    Income Component Dropdown

--- admin/src/components/Analysis/Income.js
+++ admin/src/components/Analysis/Income.js
@@ -1,13 +1,83 @@
 import React from "react";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Paper,
+  Select,
+  Typography,
+} from "@material-ui/core";
+import Line from "../ChartJS/Line";
 
 class Income extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      labels: [],
+      title: "",
+      selected: "1",
+      data: [],
+      income: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.getData(this.state.selected);
+  }
+
+  getData(value) {
+      
+  }
+
+  onChange(event) {
+    const { value } = event.target;
+    this.setState({ selected: value, labels: [] });
+    this.getData(value);
   }
 
   render() {
-    return <div></div>;
+    return (
+      <Paper className="analysis-paper">
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <Typography variant="h4" gutterBottom>
+            Income
+          </Typography>
+          <div style={{ display: "flex" }}>
+            <Typography
+              variant="h5"
+              gutterBottom
+              style={{ margin: "auto", marginLeft: "11px" }}
+            >
+              {this.state.income} $
+            </Typography>
+          </div>
+        </div>
+
+        <FormControl>
+          <InputLabel id="income-select-id">Select</InputLabel>
+          <Select
+            labelId="income-select-id"
+            value={this.state.selected}
+            onChange={this.onChange}
+          >
+            <MenuItem value="1">Weekly</MenuItem>
+            <MenuItem value="2">Montly</MenuItem>
+            <MenuItem value="3">Yearly</MenuItem>
+          </Select>
+        </FormControl>
+
+        {this.state.labels.length > 0 ? (
+          <Line
+            width="450px"
+            title={this.state.title}
+            labels={this.state.labels}
+            data={this.state.data}
+          />
+        ) : (
+          ""
+        )}
+      </Paper>
+    );
   }
 }
 
