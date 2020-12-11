845e9c58bceede9f752826e8218c24c8cd9543fc
Navonil Das <navneeladas@gmail.com>
2020-12-11 11:07:19 +0530

    Stats Component Dropdown

--- admin/src/components/Analysis/Stats.js
+++ admin/src/components/Analysis/Stats.js
@@ -1,14 +1,56 @@
-import { Paper } from "@material-ui/core";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Paper,
+  Select,
+  Typography,
+} from "@material-ui/core";
 import React from "react";
 
 class Stats extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      selected: "1",
+      labels: [],
+      title: "",
+      data: "1000",
+    };
   }
-
+  componentDidMount() {}
   render() {
-    return <Paper className="analysis-paper"></Paper>;
+    return (
+      <Paper className="analysis-paper">
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <div>
+            <Typography variant="h4" gutterBottom>
+              Stats
+            </Typography>
+
+            <FormControl>
+              <InputLabel id="stats-select-id">Select</InputLabel>
+              <Select labelId="stats-select-id" value={this.state.selected}>
+                <MenuItem value="1">Categories</MenuItem>
+                <MenuItem value="2">Chapters</MenuItem>
+                <MenuItem value="1">Courses</MenuItem>
+                <MenuItem value="3">Users</MenuItem>
+                <MenuItem value="4">Topics</MenuItem>
+                <MenuItem value="5">Videos</MenuItem>
+                <MenuItem value="6">Quizs</MenuItem>
+                <MenuItem value="6">Questions</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+
+          <div style={{ display: "flex", margin: "16px" }}>
+            <Typography variant="h4" gutterBottom style={{ margin: "auto" }}>
+              {this.state.data}
+            </Typography>
+          </div>
+        </div>
+      </Paper>
+    );
   }
 }
 
