778d5ed31929e332972a41deaf242d851353e976
Navonil Das <navneeladas@gmail.com>
2020-12-12 13:12:27 +0530

    Removing unnecessary folders

--- backend/models/Quiz.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const QuizSchema = mongoose.Schema({
-    chapter_id:{ type: mongoose.Schema.Types.ObjectId, ref: 'Chapter' },
-    title:String,
-    index:Number
-});
-
-module.exports = mongoose.model('Quiz', QuizSchema);
