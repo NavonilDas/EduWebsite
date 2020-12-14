26438eaa4cb65d61dc3e2a6e3306cf6c695d3532
Navonil Das <navneeladas@gmail.com>
2020-12-14 11:50:31 +0530

    Changing The Profile to Functional Component & Few Validation

--- admin/src/components/Profile.jsx
+++ admin/src/components/Profile.jsx
@@ -1,80 +1,115 @@
-import React from 'react';
-import { Formik, Form, Field } from 'formik';
+import React, { useState } from 'react';
+import { useFormik } from 'formik';
 import { Button, TextField, Typography } from '@material-ui/core';
 
-class Profile extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-
-        };
+function validate(values) {
+    const errors = {};
+    if (values.email === "") {
+        errors.email = 'Email Required';
+    }
+    if (values.name === "") {
+        errors.name = 'Name Required';
     }
 
-    validate(values) {
+    if (values.password === "" && values.newpassword !== "") {
+        errors.password = 'Password Required';
+    }
+    if (values.password !== "" && values.newpassword === "") {
+        errors.newpassword = 'Password Required';
     }
 
-    submit(values, data) {
-
-    }
-
-    render() {
-        return (
-            <div>
-
-                <Typography variant="h5" gutterBottom style={{ margin: "11px 0 0 11px" }}>
-                    Edit Profile
-                </Typography>
-
-                <Formik
-                    initialValues={{
-                        email: ''
-                    }}
-                    validate={this.validate.bind(this)}
-                    onSubmit={this.submit.bind(this)}
-                >
-                    <Form className="profile-form">
-                        <Field
-                            component={TextField}
-                            name="name"
-                            type="text"
-                            label="Name"
-                        />
-                        <Field
-                            component={TextField}
-                            name="email"
-                            type="email"
-                            label="Email"
-                        />
-                        <Field
-                            component={TextField}
-                            name="phone"
-                            type="tel"
-                            label="Phone Number"
-                        />
-                        <Field
-                            component={TextField}
-                            name="password"
-                            type="password"
-                            label="Current Password"
-                        />
-                        <Field
-                            component={TextField}
-                            name="newpassword"
-                            type="password"
-                            label="New Password"
-                        />
-
-                        <Button color="primary" variant="contained" fullWidth type="submit">
-                            Save Changes
-                        </Button>
-
-                    </Form>
-                </Formik>
-            </div>
-        );
-    }
+    return errors;
 }
 
+const Profile = () => {
+    const [state, setSate] = useState({
+        email: "",
+        name: '',
+        phone: '',
+        password: '',
+        newpassword: ''
+    });
+
+    // TODO: Fetch Initial Data
+
+
+    const formik = useFormik({
+        initialValues: state,
+        validate,
+        onSubmit: (values) => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+
+    return (
+        <div>
+
+            <Typography variant="h5" gutterBottom style={{ margin: "11px 0 0 11px" }}>
+                Edit Profile
+            </Typography>
+
+            <form onSubmit={formik.handleSubmit} className="profile-form">
+                <TextField
+                    fullWidth
+                    value={formik.values.name}
+                    onChange={formik.handleChange}
+                    error={formik.touched.name && Boolean(formik.errors.name)}
+                    helperText={formik.touched.name && formik.errors.name}
+                    name="name"
+                    type="text"
+                    label="Name"
+                    required
+                />
+                <TextField
+                    fullWidth
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    error={formik.touched.email && Boolean(formik.errors.email)}
+                    helperText={formik.touched.email && formik.errors.email}
+                    name="email"
+                    type="email"
+                    label="Email"
+                    required
+                />
+                <TextField
+                    fullWidth
+                    value={formik.values.phone}
+                    onChange={formik.handleChange}
+                    error={formik.touched.phone && Boolean(formik.errors.phone)}
+                    helperText={formik.touched.phone && formik.errors.phone}
+                    name="phone"
+                    type="tel"
+                    label="Phone Number"
+                />
+                <TextField
+                    fullWidth
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                    error={formik.touched.password && Boolean(formik.errors.password)}
+                    helperText={formik.touched.password && formik.errors.password}
+                    name="password"
+                    type="password"
+                    label="Current Password"
+                />
+                <TextField
+                    fullWidth
+                    value={formik.values.newpassword}
+                    onChange={formik.handleChange}
+                    error={formik.touched.newpassword && Boolean(formik.errors.newpassword)}
+                    helperText={formik.touched.newpassword && formik.errors.newpassword}
+                    name="newpassword"
+                    type="password"
+                    label="New Password"
+                />
+
+                <Button color="primary" variant="contained" fullWidth type="submit">
+                    Save Changes
+                </Button>
+
+            </form>
+        </div>
+    );
+};
 
 export default Profile;
