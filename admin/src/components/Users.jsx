c58f9da720c27a08a204cde1085ed384bb5eb657
Navonil Das <navneeladas@gmail.com>
2020-12-24 11:50:14 +0530

    Users Table Page Redesign

--- admin/src/components/Users.jsx
+++ admin/src/components/Users.jsx
@@ -17,73 +17,51 @@ class Users extends React.Component {
         this.blockUser = this.blockUser.bind(this);
         this.searchUser = this.searchUser.bind(this);
     }
+
     showBlockAlert(id) {
         this.blockId = id;
         this.setState({ block: true });
     }
+
     closeBlockAlert() {
         this.setState({ block: false });
     }
+
     blockUser() {
         console.log(this.blockId);
         this.setState({ block: false });
     }
+
     searchUser(eve) {
         const query = eve.target.value;
         if (query && query !== "") {
 
         }
     }
+
     changePage(eve, page) {
-        
+
     }
     render() {
         return (
             <main className="admin-content">
                 <NavBar title="Users" />
-                <div className="admin-body">
-                    <Dialog
-                        open={this.state.block}
-                        onClose={this.closeBlockAlert}
-                        aria-labelledby="draggable-dialog-title"
-                    >
-                        <DialogTitle style={{ cursor: 'move' }} id="draggable-dialog-title">
-                            Alert
-                        </DialogTitle>
-                        <DialogContent>
-                            <DialogContentText>
-                                Are you Sure you want to Block the User?.
-                            </DialogContentText>
-                        </DialogContent>
-                        <DialogActions>
-                            <Button autoFocus color="primary" onClick={this.closeBlockAlert}>
-                                Cancel
-                            </Button>
-                            <Button color="primary" onClick={this.blockUser}>
-                                Block
-                            </Button>
-                        </DialogActions>
-                    </Dialog>
-                    <form>
-                        <TextField id="standard-basic" label="Search Username" onChange={this.searchUser} />
-                        <FormControl variant="filled">
-                            <InputLabel id="demo-simple-select-filled-label">Filter</InputLabel>
-                            <Select
-                                labelId="demo-simple-select-filled-label"
-                                id="demo-simple-select-filled"
-                                value={0}
-                            >
-                                <MenuItem value={0}><em>ALL</em></MenuItem>
-                                <MenuItem value={10}>Ten</MenuItem>
-                                <MenuItem value={20}>Twenty</MenuItem>
-                                <MenuItem value={30}>Thirty</MenuItem>
-                            </Select>
-                        </FormControl>
-                        <Button variant="contained" color="primary">
+                <div className="admin-body d-flex flex-column" style={{ height: "100%" }}>
+
+                    <form className="d-flex" style={{ marginBottom: "11px" }}>
+                        <TextField
+                            id="standard-basic"
+                            label="Search Username"
+                            onChange={this.searchUser}
+                            style={{ flex: 1 }}
+                        />
+
+                        <Button variant="contained" color="primary" style={{ marginLeft: "11px" }}>
                             Search
                         </Button>
                     </form>
-                    <List dense>
+
+                    <List dense style={{ flex: 1 }}>
                         {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((value, i) => (
                             <ListItem key={value} button>
                                 <ListItemAvatar>
@@ -94,23 +72,50 @@ class Users extends React.Component {
                                 </ListItemAvatar>
                                 <ListItemText primary={`User Name ${value + 1}`} />
                                 <ListItemSecondaryAction>
-                                    <Select value={30}>
-                                        <MenuItem value={10}>Admin</MenuItem>
-                                        <MenuItem value={20}>Teacher</MenuItem>
-                                        <MenuItem value={30}>Student</MenuItem>
-                                    </Select>
                                     <IconButton style={{ color: "#db3825" }} edge="end" aria-label="Delete" onClick={() => this.showBlockAlert(10)}>
                                         <BlockIcon />
                                     </IconButton>
-                                    <IconButton style={{ color: "#db3825" }} edge="end" aria-label="Delete">
+                                    {/* <IconButton style={{ color: "#db3825" }} edge="end" aria-label="Delete">
                                         <DeleteIcon />
-                                    </IconButton>
+                                    </IconButton> */}
                                 </ListItemSecondaryAction>
                             </ListItem>
                         ))}
                     </List>
-                    <Pagination onChange={this.changePage} count={10} variant="outlined" color="primary" />
+
+                    <Pagination
+                        onChange={this.changePage}
+                        count={10}
+                        variant="outlined"
+                        color="primary"
+                        style={{ marginLeft: "auto" }}
+                    />
+
                 </div>
+
+                <Dialog
+                    open={this.state.block}
+                    onClose={this.closeBlockAlert}
+                    aria-labelledby="draggable-dialog-title"
+                >
+                    <DialogTitle style={{ cursor: 'move' }} id="draggable-dialog-title">
+                        Alert
+                        </DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            Are you Sure you want to Block the User?.
+                            </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button autoFocus color="primary" onClick={this.closeBlockAlert}>
+                            Cancel
+                            </Button>
+                        <Button color="primary" onClick={this.blockUser}>
+                            Block
+                            </Button>
+                    </DialogActions>
+                </Dialog>
+
             </main>
         );
     }
